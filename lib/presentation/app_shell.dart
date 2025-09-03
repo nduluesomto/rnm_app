80cfc94709a3026f287032236883b89d56d4f6c9
nduluesomto <dikenduluesomto.jp@gmail.com>
2025-09-03 16:43:13 +0300

    debugShowCheckedModeBanner = deactivated

--- lib/presentation/app_shell.dart
+++ lib/presentation/app_shell.dart
@@ -28,6 +28,7 @@ class _AppShellState extends State<AppShell> {
     return BlocBuilder<ThemeCubit, ThemeMode>(
       builder: (context, themeMode) {
         return MaterialApp(
+          debugShowCheckedModeBanner: false,
           title: 'Rick & Morty',
           theme: ThemeData.light(),
           darkTheme: ThemeData.dark(),
